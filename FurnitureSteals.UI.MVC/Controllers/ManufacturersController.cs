55c97cb97f28f19d2aac879e2f1158b6f9f2de2a
nataliekuechler <nataliekuechler@outlook.com>
2022-12-08 12:32:47 -0600

    Scaffolded Controllers/Views Added credentials and registered myself as a user

---
+++ FurnitureSteals.UI.MVC/Controllers/ManufacturersController.cs
@@ -0,0 +1,160 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.Rendering;
+using Microsoft.EntityFrameworkCore;
+using FurnitureSteals.DATA.EF.Models;
+
+namespace FurnitureSteals.UI.MVC.Controllers
+{
+    public class ManufacturersController : Controller
+    {
+        private readonly storefrontContext _context;
+
+        public ManufacturersController(storefrontContext context)
+        {
+            _context = context;
+        }
+
+        // GET: Manufacturers
+        public async Task<IActionResult> Index()
+        {
+              return View(await _context.Manufacturers.ToListAsync());
+        }
+
+        // GET: Manufacturers/Details/5
+        public async Task<IActionResult> Details(int? id)
+        {
+            if (id == null || _context.Manufacturers == null)
+            {
+                return NotFound();
+            }
+
+            var manufacturer = await _context.Manufacturers
+                .FirstOrDefaultAsync(m => m.ManufacturerId == id);
+            if (manufacturer == null)
+            {
+                return NotFound();
+            }
+
+            return View(manufacturer);
+        }
+
+        // GET: Manufacturers/Create
+        public IActionResult Create()
+        {
+            return View();
+        }
+
+        // POST: Manufacturers/Create
+        // To protect from overposting attacks, enable the specific properties you want to bind to.
+        // For more details, see http://go.microsoft.com/fwlink/?LinkId=317598.
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public async Task<IActionResult> Create([Bind("ManufacturerId,CategoryName")] Manufacturer manufacturer)
+        {
+            if (ModelState.IsValid)
+            {
+                _context.Add(manufacturer);
+                await _context.SaveChangesAsync();
+                return RedirectToAction(nameof(Index));
+            }
+            return View(manufacturer);
+        }
+
+        // GET: Manufacturers/Edit/5
+        public async Task<IActionResult> Edit(int? id)
+        {
+            if (id == null || _context.Manufacturers == null)
+            {
+                return NotFound();
+            }
+
+            var manufacturer = await _context.Manufacturers.FindAsync(id);
+            if (manufacturer == null)
+            {
+                return NotFound();
+            }
+            return View(manufacturer);
+        }
+
+        // POST: Manufacturers/Edit/5
+        // To protect from overposting attacks, enable the specific properties you want to bind to.
+        // For more details, see http://go.microsoft.com/fwlink/?LinkId=317598.
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public async Task<IActionResult> Edit(int id, [Bind("ManufacturerId,CategoryName")] Manufacturer manufacturer)
+        {
+            if (id != manufacturer.ManufacturerId)
+            {
+                return NotFound();
+            }
+
+            if (ModelState.IsValid)
+            {
+                try
+                {
+                    _context.Update(manufacturer);
+                    await _context.SaveChangesAsync();
+                }
+                catch (DbUpdateConcurrencyException)
+                {
+                    if (!ManufacturerExists(manufacturer.ManufacturerId))
+                    {
+                        return NotFound();
+                    }
+                    else
+                    {
+                        throw;
+                    }
+                }
+                return RedirectToAction(nameof(Index));
+            }
+            return View(manufacturer);
+        }
+
+        // GET: Manufacturers/Delete/5
+        public async Task<IActionResult> Delete(int? id)
+        {
+            if (id == null || _context.Manufacturers == null)
+            {
+                return NotFound();
+            }
+
+            var manufacturer = await _context.Manufacturers
+                .FirstOrDefaultAsync(m => m.ManufacturerId == id);
+            if (manufacturer == null)
+            {
+                return NotFound();
+            }
+
+            return View(manufacturer);
+        }
+
+        // POST: Manufacturers/Delete/5
+        [HttpPost, ActionName("Delete")]
+        [ValidateAntiForgeryToken]
+        public async Task<IActionResult> DeleteConfirmed(int id)
+        {
+            if (_context.Manufacturers == null)
+            {
+                return Problem("Entity set 'storefrontContext.Manufacturers'  is null.");
+            }
+            var manufacturer = await _context.Manufacturers.FindAsync(id);
+            if (manufacturer != null)
+            {
+                _context.Manufacturers.Remove(manufacturer);
+            }
+            
+            await _context.SaveChangesAsync();
+            return RedirectToAction(nameof(Index));
+        }
+
+        private bool ManufacturerExists(int id)
+        {
+          return _context.Manufacturers.Any(e => e.ManufacturerId == id);
+        }
+    }
+}
---
+++ FurnitureSteals.UI.MVC/Controllers/ProductsController.cs
@@ -0,0 +1,179 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.Rendering;
+using Microsoft.EntityFrameworkCore;
+using FurnitureSteals.DATA.EF.Models;
+
+namespace FurnitureSteals.UI.MVC.Controllers
+{
+    public class ProductsController : Controller
+    {
+        private readonly storefrontContext _context;
+
+        public ProductsController(storefrontContext context)
+        {
+            _context = context;
+        }
+
+        // GET: Products
+        public async Task<IActionResult> Index()
+        {
+            var storefrontContext = _context.Products.Include(p => p.Category).Include(p => p.Manufacturer).Include(p => p.ProductStatusNavigation);
+            return View(await storefrontContext.ToListAsync());
+        }
+
+        // GET: Products/Details/5
+        public async Task<IActionResult> Details(int? id)
+        {
+            if (id == null || _context.Products == null)
+            {
+                return NotFound();
+            }
+
+            var product = await _context.Products
+                .Include(p => p.Category)
+                .Include(p => p.Manufacturer)
+                .Include(p => p.ProductStatusNavigation)
+                .FirstOrDefaultAsync(m => m.ProductId == id);
+            if (product == null)
+            {
+                return NotFound();
+            }
+
+            return View(product);
+        }
+
+        // GET: Products/Create
+        public IActionResult Create()
+        {
+            ViewData["CategoryId"] = new SelectList(_context.Categories, "CategoryId", "CategoryId");
+            ViewData["ManufacturerId"] = new SelectList(_context.Manufacturers, "ManufacturerId", "CategoryName");
+            ViewData["ProductStatus"] = new SelectList(_context.ProductStatuses, "StatusId", "StatusName");
+            return View();
+        }
+
+        // POST: Products/Create
+        // To protect from overposting attacks, enable the specific properties you want to bind to.
+        // For more details, see http://go.microsoft.com/fwlink/?LinkId=317598.
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public async Task<IActionResult> Create([Bind("ProductId,CollectionName,ProductDescription,ManufacturerId,ProductStatus,CategoryId,Price,Quantity")] Product product)
+        {
+            if (ModelState.IsValid)
+            {
+                _context.Add(product);
+                await _context.SaveChangesAsync();
+                return RedirectToAction(nameof(Index));
+            }
+            ViewData["CategoryId"] = new SelectList(_context.Categories, "CategoryId", "CategoryId", product.CategoryId);
+            ViewData["ManufacturerId"] = new SelectList(_context.Manufacturers, "ManufacturerId", "CategoryName", product.ManufacturerId);
+            ViewData["ProductStatus"] = new SelectList(_context.ProductStatuses, "StatusId", "StatusName", product.ProductStatus);
+            return View(product);
+        }
+
+        // GET: Products/Edit/5
+        public async Task<IActionResult> Edit(int? id)
+        {
+            if (id == null || _context.Products == null)
+            {
+                return NotFound();
+            }
+
+            var product = await _context.Products.FindAsync(id);
+            if (product == null)
+            {
+                return NotFound();
+            }
+            ViewData["CategoryId"] = new SelectList(_context.Categories, "CategoryId", "CategoryId", product.CategoryId);
+            ViewData["ManufacturerId"] = new SelectList(_context.Manufacturers, "ManufacturerId", "CategoryName", product.ManufacturerId);
+            ViewData["ProductStatus"] = new SelectList(_context.ProductStatuses, "StatusId", "StatusName", product.ProductStatus);
+            return View(product);
+        }
+
+        // POST: Products/Edit/5
+        // To protect from overposting attacks, enable the specific properties you want to bind to.
+        // For more details, see http://go.microsoft.com/fwlink/?LinkId=317598.
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public async Task<IActionResult> Edit(int id, [Bind("ProductId,CollectionName,ProductDescription,ManufacturerId,ProductStatus,CategoryId,Price,Quantity")] Product product)
+        {
+            if (id != product.ProductId)
+            {
+                return NotFound();
+            }
+
+            if (ModelState.IsValid)
+            {
+                try
+                {
+                    _context.Update(product);
+                    await _context.SaveChangesAsync();
+                }
+                catch (DbUpdateConcurrencyException)
+                {
+                    if (!ProductExists(product.ProductId))
+                    {
+                        return NotFound();
+                    }
+                    else
+                    {
+                        throw;
+                    }
+                }
+                return RedirectToAction(nameof(Index));
+            }
+            ViewData["CategoryId"] = new SelectList(_context.Categories, "CategoryId", "CategoryId", product.CategoryId);
+            ViewData["ManufacturerId"] = new SelectList(_context.Manufacturers, "ManufacturerId", "CategoryName", product.ManufacturerId);
+            ViewData["ProductStatus"] = new SelectList(_context.ProductStatuses, "StatusId", "StatusName", product.ProductStatus);
+            return View(product);
+        }
+
+        // GET: Products/Delete/5
+        public async Task<IActionResult> Delete(int? id)
+        {
+            if (id == null || _context.Products == null)
+            {
+                return NotFound();
+            }
+
+            var product = await _context.Products
+                .Include(p => p.Category)
+                .Include(p => p.Manufacturer)
+                .Include(p => p.ProductStatusNavigation)
+                .FirstOrDefaultAsync(m => m.ProductId == id);
+            if (product == null)
+            {
+                return NotFound();
+            }
+
+            return View(product);
+        }
+
+        // POST: Products/Delete/5
+        [HttpPost, ActionName("Delete")]
+        [ValidateAntiForgeryToken]
+        public async Task<IActionResult> DeleteConfirmed(int id)
+        {
+            if (_context.Products == null)
+            {
+                return Problem("Entity set 'storefrontContext.Products'  is null.");
+            }
+            var product = await _context.Products.FindAsync(id);
+            if (product != null)
+            {
+                _context.Products.Remove(product);
+            }
+            
+            await _context.SaveChangesAsync();
+            return RedirectToAction(nameof(Index));
+        }
+
+        private bool ProductExists(int id)
+        {
+          return _context.Products.Any(e => e.ProductId == id);
+        }
+    }
+}
